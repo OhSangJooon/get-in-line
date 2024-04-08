3bc682014b0626b743fc950b362a061dd8e44c84
OhSangJooon <dhdldl1@naver.com>
2024-04-08 17:31:58 +0900

    타임리프 디커플 로직 false로 변경

--- src/main/java/com/dean/getinline/config/ThymeleafConfig.java
+++ src/main/java/com/dean/getinline/config/ThymeleafConfig.java
@@ -2,23 +2,20 @@ package com.dean.getinline.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 
 @RequiredArgsConstructor
 @Configuration
 public class ThymeleafConfig {
 
+    // 타임리프 디커플 로직을 활성화 하기 위한 Bean
     @Bean
     public SpringResourceTemplateResolver thymeleafTemplateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        // 템플릿 파일이 위치한 경로 설정
-        templateResolver.setPrefix("/WEB-INF/templates/");
-        // 템플릿 파일 확장자 설정
-        templateResolver.setSuffix(".html");
-        // 템플릿 모드 설정
-        templateResolver.setTemplateMode("HTML");
         // 디커플 로직 설정
-        templateResolver.setUseDecoupledLogic(true);
+        templateResolver.setUseDecoupledLogic(false);
         return templateResolver;
     }
 
