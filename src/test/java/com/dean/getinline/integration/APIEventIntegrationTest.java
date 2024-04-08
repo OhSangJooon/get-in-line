e41f08719cdfd5e2275dc91eea7c75b2c27b5a97
OhSangJooon <dhdldl1@naver.com>
2024-04-08 16:17:32 +0900

    integration Test 추가 / 비즈니스 로직 마지막

---
+++ src/test/java/com/dean/getinline/integration/APIEventIntegrationTest.java
@@ -0,0 +1,54 @@
+package com.dean.getinline.integration;
+
+import com.dean.getinline.constant.ErrorCode;
+import com.dean.getinline.constant.EventStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+/**
+ * 통합테스트 IntegrationTest를 하기위한 간단한 방법
+ * */
+@AutoConfigureMockMvc
+@SpringBootTest
+public class APIEventIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void integrationTest() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/events")
+                        .queryParam("placeId", "1")
+                        .queryParam("eventName", "운동")
+                        .queryParam("eventStatus", EventStatus.OPENED.name())
+                        .queryParam("eventStartDateTime", "2021-01-01T00:00:00")
+                        .queryParam("eventEndDateTime", "2021-01-02T00:00:00")
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.data").isEmpty())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.errorCode").value(ErrorCode.OK.getCode()))
+                .andExpect(jsonPath("$.message").value(ErrorCode.OK.getMessage()))
+                .andDo(print());
+    }
+}
